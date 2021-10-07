8f841a61b9b507c507c2b5eaeba80285cd7f1d28
analista_laborfinanciera <analista_sistemas@laborfinanciera.com>
2021-10-07 17:36:11 -0500

    ajuste de firmas

--- data/utils/helpers.ts
+++ data/utils/helpers.ts
@@ -1,6 +1,6 @@
 export default{
     //apiUrl: 'https://prod.rqprate.com/',
-    apiUrl: 'https://2537-2800-484-7f81-e13c-95e1-d859-849b-28ed.ngrok.io/',
+    apiUrl: 'https://dev.rqprate.com/',
     getCasoByToken: 'api/ado/retrieve-data-case',
     verifyOTPCode: 'api/ado/validate-otp',
     againOTPCode: 'api/ado/send-code',
