472f5f24a0cd29b64fa99e1853c63b8cd6af3b16
analista_laborfinanciera <analista_sistemas@laborfinanciera.com>
2021-12-14 17:49:55 -0500

    ajustes visuales

--- components/Modals/otpModal.tsx
+++ components/Modals/otpModal.tsx
@@ -181,12 +181,13 @@ export default function OtpModal({ token }: Props): ReactElement {
                             </div>
                         </div>
                     </div>
+                    <br/>
                     <div className="modal-footer center-content">
                         <button
                             onClick={alertConfirm}
-                            className="btn-firma btn-firma-disable" type="button"><svg xmlns="http://www.w3.org/2000/svg"
+                            className="btn-limpiar btn-firma-disable" type="button"><svg xmlns="http://www.w3.org/2000/svg"
                                                                                        className="icon icon-tabler icon-tabler-device-mobile-message" width="24" height="24"
-                                                                                       viewBox="0 0 24 24" strokeWidth="2" stroke="#ffffff" fill="none"
+                                                                                       viewBox="0 0 24 24" strokeWidth="2" stroke="#000000" fill="none"
                                                                                        strokeLinecap="round" strokeLinejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                             <path d="M11 3h10v8h-3l-4 2v-2h-3z"/>
@@ -196,9 +197,9 @@ export default function OtpModal({ token }: Props): ReactElement {
                             </button>
                         <button 
                         onClick={verifyOtp}
-                        className="btn-limpiar btn-firma-disable" type="button"> Validar <svg
+                        className="btn-firma btn-firma-disable" type="button"> Validar Código<svg
                             xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-chevrons-right"
-                            width="24" height="24" viewBox="0 0 24 24" strokeWidth="3" stroke="#000000" fill="none"
+                            width="24" height="24" viewBox="0 0 24 24" strokeWidth="3" stroke="#ffffff" fill="none"
                             strokeLinecap="round" strokeLinejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                             <polyline points="7 7 12 12 7 17"/>
